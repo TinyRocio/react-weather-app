fee3375bd7837995f63c433ec239cc896d8bf72e
TinyRocio <78389359+TinyRocio@users.noreply.github.com>
2021-04-30 12:55:48 -0400

    added open sourced code link

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <p>
-      <a href="/"> Open-sourced Code</a>
+      <a href="https://github.com/TinyRocio/react-weather-app"> Open-sourced Code</a>
       by Rocio Valenzuela
     </p>
   );
